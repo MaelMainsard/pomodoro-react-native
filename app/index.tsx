78dc638813bd3d43d273423b388f8f5d19ccc19e
MaelMainsard <mainsardm@gmail.com>
2024-10-08 14:42:41 +0200

    Le home screen est bon

---
+++ app/index.tsx
@@ -0,0 +1,51 @@
+import { ThemedView } from "@/components/ThemedView";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function HomeScreen() {
+    const router = useRouter();
+
+    return (
+        <ThemedView style={styles.screen}>
+            <View style={styles.buttons_group}>
+                <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={() => router.push({ pathname: '/timer', params: { mode: 45/15 } })}
+                >
+                    <Text style={styles.buttonText}>45/15</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={() => router.push({ pathname: '/timer', params: { mode: 25/5 } })}
+                >
+                    <Text style={styles.buttonText}>25/5</Text>
+                </TouchableOpacity>
+            </View>
+        </ThemedView>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        height: '100%',
+    },
+    buttons_group: {
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        width: '80%',
+        marginVertical: 10,
+        backgroundColor: '#007BFF',
+        borderRadius: 10,
+        paddingVertical: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 18,
+        textAlign: 'center',
+    },
+});
